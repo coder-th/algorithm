90af8b30b8eb197e2393f9ddb96f2522066ec95e
天衡 <13265317640@163.com>
2021-08-31 09:20:51 +0800

    fix: 修复计数排序负数的情况

--- 排序算法/8.计数排序.js
+++ 排序算法/8.计数排序.js
@@ -3,26 +3,28 @@ function countingSort(arr) {
     return arr;
   }
   var max = Number.MIN_VALUE;
+  var min = 0; // min => 作用就是可以兼容负数的情况，达到扩容的目的
   for (var i = 0; i < arr.length; i++) {
-    // 计算出额外空间的最大容量
+    // 计算出数组的最大值和最小值
     max = Math.max(max, arr[i]);
+    min = Math.min(min, arr[i]);
   }
-  var bucket = new Array(max + 1).fill(0);
+  var bucket = new Array(max - min + 1).fill(0);
   for (var i = 0; i < arr.length; i++) {
     // 每一个桶的索引代表该桶指向原数组的值，存储的值为该元素出现的个数
-    bucket[arr[i]]++;
+    bucket[arr[i] - min]++;
   }
   var i = 0;
   for (var j = 0; j < bucket.length; j++) {
     while (bucket[j]--) {
       // 将该桶的存储的count值反向输出,比如   bucket[50] = 3   => newArr:[50,50,50]
-      arr[i++] = j;
+      arr[i++] = j + min;
     }
   }
 
   return arr;
 }
 console.log(
-  countingSort([3, 5, 38, 15, 36, 26, 27, 2, 44, 46, 4, 19, 47, 48, 50])
-  //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
+  countingSort([3, 5, 38, 15, -6, 0, 36, 26, -27, 2, 44, 46, 4, 19, 47, 48, 50])
+  //  [-27, -6, 0 ,  2,  3,  4,  5,15, 19, 26, 36, 38, 44,46, 47, 48, 50]
 );
