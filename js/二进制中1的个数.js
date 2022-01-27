aa3ce1a5c4319f5c06682d2791d3b1e4783142f0
tianheng890 <13265317640@163.com>
2022-01-27 16:32:39 +0800

    二进制中的个数

---
+++ js/二进制中1的个数.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let result = 0;
+  while (n) {
+    n &= n - 1;
+    result++;
+  }
+  return result;
+};
